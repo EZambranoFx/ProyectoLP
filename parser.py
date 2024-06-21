82dc55b6ebb2c45cd004b096798427acf46394c3
Enrique Zambrano <josezamb@espol.edu.ec>
2024-06-21 01:27:32 -0500

    ED, EC Y FUNCT Enrique Zambrano

---
+++ parser.py
@@ -0,0 +1,43 @@
+import ply.yacc as yacc
+from lexer import tokens
+import datetime
+import os
+
+# Inicio - Enrique Zambrano 
+
+# Estructura de Datos: Declaracion de objetos
+def p_object_declaration(p):
+    '''object_declaration : VAR VARIABLE EQUALS NEW CLASS LPAREN RPAREN SEMI'''
+
+# Estructura de Control: While
+def p_while_statement(p):
+    '''while_statement : WHILE LPAREN expression RPAREN statement'''
+
+# Funcion: Funcion Variable
+def p_function_variable(p):
+    '''function_variable : FUNCTION VARIABLE LPAREN RPAREN LBRACE statements RBRACE'''
+
+#Fin - Enrique Zambrano
+
+#-----------TODO PRATT & ALEJANDRO------------
+
+parser = yacc.yacc()
+
+
+def test_parser(data, username):
+    result = parser.parse(data)
+    log_folder = "logs"
+    os.makedirs(log_folder, exist_ok=True)
+    current_time = datetime.datetime.now().strftime("%d%m%Y-%Hh%M")
+    log_filename = f"sintactico-{username}-{current_time}.txt"
+    log_path = os.path.join(log_folder, log_filename)
+
+    with open(log_path, "w") as log_file:
+        if result:
+            log_file.write(str(result))
+            print(result)
+        else:
+            log_file.write("Syntax errors found")
+            print("Syntax errors found")
+
+
